add53c2e3c428afd7ff68d4092b80444a0d231c0
Steve <84108749+Scurvyez@users.noreply.github.com>
2024-05-14 14:39:49 -0400

    Initial commit

---
+++ Source/BOTW/ThingComps/CompAnimalColorRandomizer.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+
+namespace BOTW
+{
+    public class CompAnimalColorRandomizer : ThingComp
+    {
+        public CompProperties_AnimalColorRandomizer Props => (CompProperties_AnimalColorRandomizer)props;
+
+        public Color newColor = new ();
+        public Color newColorTwo = new ();
+        private readonly float alpha = 1f;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            if (!respawningAfterLoad && parent is Pawn pawn)
+            {
+                if (pawn.gender == Gender.Male)
+                {
+                    newColor = new Color(Props.maleRRangeOne.RandomInRange, Props.maleGRangeOne.RandomInRange, Props.maleBRangeOne.RandomInRange, alpha);
+                    newColorTwo = new Color(Props.maleRRangeTwo.RandomInRange, Props.maleGRangeTwo.RandomInRange, Props.maleBRangeTwo.RandomInRange, alpha);
+                }
+                else if (pawn.gender == Gender.Female)
+                {
+                    newColor = new Color(Props.femaleRRangeOne.RandomInRange, Props.femaleGRangeOne.RandomInRange, Props.femaleBRangeOne.RandomInRange, alpha);
+                    newColorTwo = new Color(Props.femaleRRangeTwo.RandomInRange, Props.femaleGRangeTwo.RandomInRange, Props.femaleBRangeTwo.RandomInRange, alpha);
+                }
+                else
+                {
+                    newColor = Color.white;
+                    newColorTwo = Color.white;
+                }
+                pawn.Drawer.renderer.SetAllGraphicsDirty();
+            }
+        }
+
+        public override void PostExposeData()
+        {
+            base.PostExposeData();
+            Scribe_Values.Look(ref newColor, "newColor");
+            Scribe_Values.Look(ref newColorTwo, "newColorTwo");
+        }
+    }
+
+    public class CompProperties_AnimalColorRandomizer : CompProperties
+    {
+        public FloatRange maleRRangeOne;
+        public FloatRange maleGRangeOne;
+        public FloatRange maleBRangeOne;
+        public FloatRange maleRRangeTwo;
+        public FloatRange maleGRangeTwo;
+        public FloatRange maleBRangeTwo;
+
+        public FloatRange femaleRRangeOne;
+        public FloatRange femaleGRangeOne;
+        public FloatRange femaleBRangeOne;
+        public FloatRange femaleRRangeTwo;
+        public FloatRange femaleGRangeTwo;
+        public FloatRange femaleBRangeTwo;
+
+        public CompProperties_AnimalColorRandomizer()
+        {
+            compClass = typeof(CompAnimalColorRandomizer);
+        }
+
+        public override IEnumerable<string> ConfigErrors(ThingDef parentDef)
+        {
+            foreach (string error in base.ConfigErrors(parentDef))
+            {
+                yield return error;
+            }
+            if (maleRRangeOne == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] maleRRangeOne is null. Please provide a range.");
+            }
+            if (maleGRangeOne == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] maleGRangeOne is null. Please provide a range.");
+            }
+            if (maleBRangeOne == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] maleBRangeOne is null. Please provide a range.");
+            }
+            if (maleRRangeTwo == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] maleRRangeTwo is null. Please provide a range.");
+            }
+            if (maleGRangeTwo == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] maleGRangeTwo is null. Please provide a range.");
+            }
+            if (maleBRangeTwo == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] maleBRangeTwo is null. Please provide a range.");
+            }
+            if (femaleRRangeOne == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] femaleRRangeOne is null. Please provide a range.");
+            }
+            if (femaleGRangeOne == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] femaleGRangeOne is null. Please provide a range.");
+            }
+            if (femaleBRangeOne == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] femaleBRangeOne is null. Please provide a range.");
+            }
+            if (femaleRRangeTwo == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] femaleRRangeTwo is null. Please provide a range.");
+            }
+            if (femaleGRangeTwo == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] femaleGRangeTwo is null. Please provide a range.");
+            }
+            if (femaleBRangeTwo == null)
+            {
+                BOTWLog.Warning("[CompProperties_AnimalColorRandomizer] femaleBRangeTwo is null. Please provide a range.");
+            }
+        }
+    }
+}
---
+++ Source/BOTW/Utils/BOTWLog.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace BOTW
+{
+    public static class BOTWLog
+    {
+        public static Color ErrorMsgCol = new(0.4f, 0.54902f, 1.0f);
+        public static Color WarningMsgCol = new(0.70196f, 0.4f, 1.0f);
+        public static Color MessageMsgCol = new(0.4f, 1.0f, 0.54902f);
+
+        public static void Error(string msg)
+        {
+            Log.Error("[BOTW] ".Colorize(ErrorMsgCol) + msg);
+        }
+
+        public static void Warning(string msg)
+        {
+            Log.Warning("[BOTW] ".Colorize(WarningMsgCol) + msg);
+        }
+
+        public static void Message(string msg)
+        {
+            Log.Message("[BOTW] ".Colorize(MessageMsgCol) + msg);
+        }
+    }
+}
